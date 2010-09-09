375f032d6e4b327dc74b5ceb716df54e7f80e734
Fabian Knittel <fabian.knittel@avona.com>
2010-09-09 02:26:00 +0200

    Switch mouse pointer to pulsing white circle

--- src/com/avona/games/towerdefence/awt/GraphicsEngine.java
+++ src/com/avona/games/towerdefence/awt/GraphicsEngine.java
@@ -170,30 +170,10 @@ public class GraphicsEngine implements GLEventListener {
 
 	public void renderMouse() {
 		final Point2d p = game.mouse.location;
-		final double width = 0.1;
-
-		Tower t = game.closestTowerWithinRadius(p, width);
-		if (t != null) {
-			gl.glColor3d(1.0, 1.0, 1.0);
-			drawCircle(t.location.x, t.location.y, 100, t.range);
-		}
-
-		gl.glPushMatrix();
-		gl.glTranslated(p.x, p.y, 0.0);
-		gl.glScaled(0.5 + 0.5 * Math.abs(Math.sin(graphicsTime.clock)),
-				0.5 + 0.5 * Math.abs(Math.sin(graphicsTime.clock)), 1);
-		gl.glRotated(Math.sin(graphicsTime.clock) * 360, 0, 0, 1);
-
-		gl.glBegin(GL.GL_QUADS);
-		gl.glColor3d(0.0, 0.5 + 0.5 * Math.abs(Math.sin(graphicsTime.clock)),
-				0.0);
-		gl.glVertex2d(width / 2, width / 2);
-		gl.glVertex2d(width / 2, -width / 2);
-		gl.glVertex2d(-width / 2, -width / 2);
-		gl.glColor3d(0.0, 0.0, 1.0);
-		gl.glVertex2d(-width / 2, width / 2);
-		gl.glEnd();
-		gl.glPopMatrix();
+		final double col = 0.4 + 0.6 * Math.abs(Math
+				.sin(2 * graphicsTime.clock));
+		gl.glColor3d(col, col, col);
+		drawFilledCircle(p.x, p.y, game.mouse.radius);
 	}
 
 	@Override
