163315c3ae0ac59ebf65e3dcb7189e4587365913
Fabian Knittel <fabian.knittel@avona.com>
2010-09-10 05:52:42 +0200

    android: we might want to store / restore our game state at some point ...

--- android/src/com/avona/games/towerdefence/android/MainActivity.java
+++ android/src/com/avona/games/towerdefence/android/MainActivity.java
@@ -1,5 +1,7 @@
 package com.avona.games.towerdefence.android;
 
+import com.avona.games.towerdefence.Util;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -7,24 +9,36 @@ public class MainActivity extends Activity {
 	private MainLoop ml;
 
 	/** Called when the activity is first created. */
-	@Override        
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
+		if (savedInstanceState == null) {
+			Util.log("creating fresh instance");
+		} else {
+			// TODO Restore game state (using Bundles).
+			Util.log("would have restored instance");
+		}
 		ml = new MainLoop(this);
 	}
-	
-    @Override
-    protected void onPause() {
-        super.onPause();
-        ml.inputActor.onPause();
-        ml.surfaceView.onPause();
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        ml.inputActor.onResume();
-        ml.surfaceView.onResume();
-    }
+	@Override
+	protected void onPause() {
+		super.onPause();
+		ml.inputActor.onPause();
+		ml.surfaceView.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		ml.inputActor.onResume();
+		ml.surfaceView.onResume();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		// TODO Store the game state (using Bundles)
+		Util.log("supposed to save instance state");
+	}
 }
