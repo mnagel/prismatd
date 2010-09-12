0534ad597cd07b1f5371607610f430e34862a87b
Fabian Knittel <fabian.knittel@avona.com>
2010-09-12 14:44:43 +0200

    Remove cruft from Util and World

--- src/com/avona/games/towerdefence/World.java
+++ src/com/avona/games/towerdefence/World.java
@@ -2,24 +2,12 @@ package com.avona.games.towerdefence;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
 
 public class World {
 	public final static float ORIGIN_X = 0;
 	public final static float WIDTH = 675;
 	public final static float ORIGIN_Y = 0;
 	public final static float HEIGHT = 480;
-
-	public static Random rand = new java.util.Random();
-
-	public VelocityVector getRandomDirection(V2 location) {
-		float x = rand.nextFloat() - 0.5f;
-		float y = rand.nextFloat() - 0.5f;
-		V2 dir = new V2(x, y);
-		dir.normalize();
-		float s = rand.nextFloat() * 75;
-		return new VelocityVector(dir, s);
-	}
 
 	public World() {
 		waypoints.clear();
