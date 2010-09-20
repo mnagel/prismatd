90b9fdf7ec250e679b9d099d9c107b68e66ab0a7
Philipp Kern <phil@philkern.de>
2010-09-20 11:57:51 +0200

    PortableGraphicsEngine: different line drawing, different colours

--- src/com/avona/games/towerdefence/PortableGraphicsEngine.java
+++ src/com/avona/games/towerdefence/PortableGraphicsEngine.java
@@ -3,6 +3,8 @@ package com.avona.games.towerdefence;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public abstract class PortableGraphicsEngine {
 
@@ -92,7 +94,8 @@ public abstract class PortableGraphicsEngine {
 
 		vertexBuffer.put((float) (gameLayer.virtualRegion.x));
 		vertexBuffer.put((float) (gameLayer.virtualRegion.y));
-		colorBuffer.put(new float[] { 0.35f, 0.82f, 0.90f, 1.0f });
+		//colorBuffer.put(new float[] { 0.35f, 0.82f, 0.90f, 1.0f });
+		colorBuffer.put(new float[] { 0.00f, 0.00f, 0.00f, 1.0f });
 
 		vertexBuffer.put((float) (gameLayer.virtualRegion.x));
 		vertexBuffer.put(0.0f);
@@ -114,29 +117,43 @@ public abstract class PortableGraphicsEngine {
 		// Draw the waypoints... top first...
 		assert (vertexBuffer.capacity() >= game.world.waypoints.size() * 2);
 		assert (colorBuffer.capacity() >= game.world.waypoints.size() * 4);
-		for (V2 p : game.world.waypoints) {
-			vertexBuffer.put(p.x + 4.0f);
-			vertexBuffer.put(p.y + 4.0f);
-			colorBuffer.put(new float[] { 0.0f, 0.0f, 0.0f, 1.0f });
+		assert (game.world.waypoints.size() > 1);
+		
+		// Start the triangle strip by drawing two points at the first
+		// waypoint.  TODO: This assumes that it's always starting at
+		// the top!
+		vertexBuffer.position(0);
+		colorBuffer.position(0);
+		final ArrayList<V2> waypoints = game.world.waypoints;
+		int vertices = 0;
+		V2 wp = waypoints.get(0);
+		vertexBuffer.put(wp.x - 4.0f);
+		vertexBuffer.put(wp.y);
+		colorBuffer.put(new float[] { 1.0f, 1.0f, 1.0f, 1.0f });
+		++vertices;
+		vertexBuffer.put(wp.x + 4.0f);
+		vertexBuffer.put(wp.y);
+		++vertices;
+		colorBuffer.put(new float[] { 1.0f, 1.0f, 1.0f, 1.0f });
+
+		for(int i = 1; i < waypoints.size(); ++i) {
+			wp = waypoints.get(i);
+			V2 previousWP = waypoints.get(i - 1);
+			float dx = previousWP.x - wp.x > 0 ? -4.0f : 4.0f;
+			float dy = previousWP.y - wp.y > 0 ? -4.0f : 4.0f;
+			vertexBuffer.put(wp.x + dx);
+			vertexBuffer.put(wp.y + dy);
+			colorBuffer.put(new float[] { 1.0f, 1.0f, 1.0f, 1.0f });
+			++vertices;
+			vertexBuffer.put(wp.x - dx);
+			vertexBuffer.put(wp.y - dy);
+			colorBuffer.put(new float[] { 1.0f, 1.0f, 1.0f, 1.0f });
+			++vertices;
 		}
 		vertexBuffer.position(0);
 		colorBuffer.position(0);
 
-		drawLine(game.world.waypoints.size(), vertexBuffer, colorBuffer);
-
-		// ...then bottom...
-		vertexBuffer.position(0);
-		colorBuffer.position(0);
-		for (V2 p : game.world.waypoints) {
-			vertexBuffer.put(p.x - 4.0f);
-			vertexBuffer.put(p.y - 4.0f);
-			colorBuffer.put(new float[] { 0.0f, 0.0f, 0.0f, 1.0f });
-		}
-
-		vertexBuffer.position(0);
-		colorBuffer.position(0);
-
-		drawLine(game.world.waypoints.size(), vertexBuffer, colorBuffer);
+		drawTriangleStrip(vertices, vertexBuffer, colorBuffer);
 	}
 
 	protected void renderMenu() {
@@ -145,24 +162,41 @@ public abstract class PortableGraphicsEngine {
 
 		vertexBuffer.put((float) (menuLayer.virtualRegion.x));
 		vertexBuffer.put((float) (menuLayer.virtualRegion.y));
-		colorBuffer.put(new float[] { 0.9f, 1.0f, 0.0f, 1.0f });
+		//colorBuffer.put(new float[] { 0.3568f, 0.1019f, 0.2117f, 1.0f });
+		colorBuffer.put(new float[] { 0.2314f, 0.4275f, 0.8980f, 1.0f });
 
 		vertexBuffer.put((float) (menuLayer.virtualRegion.x));
 		vertexBuffer.put((float) (0));
-		colorBuffer.put(new float[] { 0.9f, 1.0f, 0.0f, 1.0f });
+		colorBuffer.put(new float[] { 0.4627f, 0.6863f, 0.9372f, 1.0f });
 
 		vertexBuffer.put((float) (0));
 		vertexBuffer.put((float) (menuLayer.virtualRegion.y));
-		colorBuffer.put(new float[] { 0.9f, 1.0f, 0.0f, 1.0f });
+		colorBuffer.put(new float[] { 0.2314f, 0.4275f, 0.8980f, 1.0f });
 
 		vertexBuffer.put((float) (0));
 		vertexBuffer.put((float) (0));
-		colorBuffer.put(new float[] { 0.9f, 1.0f, 0.0f, 1.0f });
+		colorBuffer.put(new float[] { 0.4627f, 0.6863f, 0.9372f, 1.0f });
 
 		vertexBuffer.position(0);
 		colorBuffer.position(0);
 
 		drawTriangleStrip(4, vertexBuffer, colorBuffer);
+		
+		vertexBuffer.position(0);
+		colorBuffer.position(0);
+		
+		vertexBuffer.put(0.0f);
+		vertexBuffer.put(menuLayer.virtualRegion.y);
+		colorBuffer.put(new float[] { 0.0f, 0.0f, 0.0f, 1.0f });
+		
+		vertexBuffer.put(0.0f);
+		vertexBuffer.put(0.0f);
+		colorBuffer.put(new float[] { 0.0f, 0.0f, 0.0f, 1.0f });
+
+		vertexBuffer.position(0);
+		colorBuffer.position(0);
+		
+		drawLine(2, vertexBuffer, colorBuffer);
 	}
 
 	protected FloatBuffer allocateFloatBuffer(final int entries) {
