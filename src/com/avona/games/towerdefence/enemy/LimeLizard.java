833048b1992e2b3877c9bf8619da93054ddb0f0d
Michael Nagel <michael.nagel@devzero.de>
2010-10-01 15:12:33 +0200

    improve subclassing of enemies

--- src/com/avona/games/towerdefence/enemy/LimeLizard.java
+++ src/com/avona/games/towerdefence/enemy/LimeLizard.java
@@ -5,13 +5,14 @@ import com.avona.games.towerdefence.V2;
 import com.avona.games.towerdefence.world.World;
 
 public class LimeLizard extends Enemy {
-
 	private static final long serialVersionUID = 3102974967310386039L;
 
 	public LimeLizard(World world, V2 location, int level) {
 		super(world, location, level);
-		this.life = new RGB(0, 50 * level + 10, 0);
-		this.maxLife = new RGB(0, 50 * level + 10, 0);
 	}
-
+	
+	@Override
+	public RGB getMaxLife() {
+		return new RGB(0, 50 * level + 10, 0);
+	}
 }
