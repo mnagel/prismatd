mnagel/prismatd

commit 53c31726892485927b4694a89062806092f83324

Properly free and re-use vertices on GL restart.

src/com/avona/games/towerdefence/gfx/PortableGraphicsEngine.java
@@ -48,8 +48,8 @@ public abstract class PortableGraphicsEngine {
 	public void onNewScreenContext() {
 		// Make sure that the VertexArrays are cleared on a screen context
 		// reset. Otherwise, any preloaded texture wouldn't be reloaded again.
-		levelVertices = null;
-		menuVertices = null;
+		freeLevelVertices();
+		freeMenuVertices();
 	}
 
 	public abstract Texture allocateTexture();
@@ -129,13 +129,7 @@ public abstract class PortableGraphicsEngine {
 	}
 
 	protected void createLevel() {
-		if (levelVertices != null) {
-			// In case we're recreating the world, allow re-using of the
-			// buffers.
-			for (VertexArray va : levelVertices) {
-				va.freeBuffers();
-			}
-		}
+		freeLevelVertices();
 		levelVertices = new VertexArray[1];
 
 		VertexArray va = new VertexArray();
@@ -155,6 +149,16 @@ public abstract class PortableGraphicsEngine {
 		levelVertices[0] = va;
 	}
 
+	protected void freeLevelVertices() {
+		if (levelVertices != null) {
+			// In case we're recreating the world, allow re-using of the
+			// buffers.
+			for (VertexArray va : levelVertices) {
+				va.freeBuffers();
+			}
+		}
+	}
+
 	protected void renderLevel() {
 		if (levelVertices == null) {
 			createLevel();
@@ -166,6 +170,7 @@ public abstract class PortableGraphicsEngine {
 	}
 
 	protected void buildMenu() {
+		freeMenuVertices();
 		menuVertices = new VertexArray[1];
 
 		VertexArray va = new VertexArray();
@@ -185,6 +190,16 @@ public abstract class PortableGraphicsEngine {
 
 		drawVertexArray(va);
 		menuVertices[0] = va;
+	}
+
+	protected void freeMenuVertices() {
+		if (menuVertices != null) {
+			// In case we're recreating the world, allow re-using of the
+			// buffers.
+			for (VertexArray va : menuVertices) {
+				va.freeBuffers();
+			}
+		}
 	}
 
 	protected void renderMenu() {

commit 07fd9a1d2d49e8ca5b9d3feb7dcfb29ea6d156ee

Fixed regression from 53c317268 by properly nulling the array pointers.

issues/issue-d66926ed42ea24b0e0ff8c60759000b96ce0de49.yaml
@@ -5,8 +5,8 @@ type: :bugfix
 component: android
 release: 
 reporter: Michael Nagel <m.nagel@devzero.de>
-status: :unstarted
-disposition: 
+status: :closed
+disposition: :fixed
 creation_time: 2010-10-03 15:56:03.325157 Z
 references: []
 
@@ -16,3 +16,7 @@ log_events:
   - Michael Nagel <m.nagel@devzero.de>
   - created
   - ""
+- - 2010-10-03 22:54:14.575073 Z
+  - Fabian Knittel <fabian.knittel@avona.com>
+  - closed with disposition fixed
+  - Fixed regression from 53c317268 by properly nulling the array pointers.

src/com/avona/games/towerdefence/gfx/PortableGraphicsEngine.java
@@ -156,6 +156,7 @@ public abstract class PortableGraphicsEngine {
 			for (VertexArray va : levelVertices) {
 				va.freeBuffers();
 			}
+			levelVertices = null;
 		}
 	}
 
@@ -199,6 +200,7 @@ public abstract class PortableGraphicsEngine {
 			for (VertexArray va : menuVertices) {
 				va.freeBuffers();
 			}
+			menuVertices = null;
 		}
 	}
 