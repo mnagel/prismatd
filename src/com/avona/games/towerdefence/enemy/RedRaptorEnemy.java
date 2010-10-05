15cd464f79d559850846c1e95a071f52fed4cbdd
Michael Nagel <michael.nagel@devzero.de>
2010-10-05 19:34:35 +0200

    add red enemy

---
+++ src/com/avona/games/towerdefence/enemy/RedRaptorEnemy.java
@@ -0,0 +1,22 @@
+package com.avona.games.towerdefence.enemy;
+
+import com.avona.games.towerdefence.RGB;
+import com.avona.games.towerdefence.level.Level;
+
+public class RedRaptorEnemy extends Enemy {
+	private static final long serialVersionUID = 3102974973240386039L;
+
+	public RedRaptorEnemy(Level level, int levelNum) {
+		super(level, levelNum, 3 + (levelNum - 1), 
+				new RGB(50 * levelNum + 10, 0, 0), 80 + 3 * (levelNum - 1), 6);
+	}
+
+	public RedRaptorEnemy(RedRaptorEnemy other) {
+		super(other);
+	}
+
+	@Override
+	public Enemy copy() {
+		return new RedRaptorEnemy(this);
+	}
+}
