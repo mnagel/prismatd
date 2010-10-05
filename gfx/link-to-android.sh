fb1a149c4261bde88eb43e72b7e0be0c99a9a54b
Michael Nagel <michael.nagel@devzero.de>
2010-10-05 23:03:00 +0200

    add script to ease work when adding resources to android

---
+++ gfx/link-to-android.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+echo "known problems: does not remove cruft"
+echo "does not validate filenames. filenames must not contain -"
+echo "does not necessarily link all needed files"
+echo ""
+
+shopt -s extglob
+for i in *[lr]*([0-9]).png icon.png
+do
+# put links in the android res directory
+ln -s ./../../../gfx/$i ./../android/res/drawable/$i
+done
+
+echo ""
+echo "DO NOT FORGET TO DELETE R.java in ECLIPLSE"
+echo "DO NOT FORGET TO DELETE R.java in ECLIPLSE"
+echo "DO NOT FORGET TO DELETE R.java in ECLIPLSE"
