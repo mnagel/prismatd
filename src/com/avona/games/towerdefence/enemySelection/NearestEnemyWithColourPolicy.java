6403f0bc546cd6627f415162b1c667606cd0c052
Fabian Knittel <fabian.knittel@avona.com>
2010-10-06 00:09:52 +0200

    Towers of level > 1 now support NearestEnemyWithColourPolicy
    (Unfortunately, there's no way to build Towers of level > 1 yet.)

--- src/com/avona/games/towerdefence/enemySelection/NearestEnemyWithColourPolicy.java
+++ src/com/avona/games/towerdefence/enemySelection/NearestEnemyWithColourPolicy.java
@@ -2,11 +2,29 @@ package com.avona.games.towerdefence.enemySelection;
 
 import java.util.List;
 
+import com.avona.games.towerdefence.RGB;
 import com.avona.games.towerdefence.enemy.Enemy;
 import com.avona.games.towerdefence.tower.Tower;
 
-public class NearestEnemyPolicy implements EnemySelectionPolicy {
+public class NearestEnemyWithColourPolicy implements EnemySelectionPolicy {
 	private static final long serialVersionUID = 1L;
+
+	private boolean withRed;
+	private boolean withGreen;
+	private boolean withBlue;
+
+	public NearestEnemyWithColourPolicy(boolean withRed, boolean withGreen,
+			boolean withBlue) {
+		this.withRed = withRed;
+		this.withGreen = withGreen;
+		this.withBlue = withBlue;
+	}
+
+	final private boolean enemyHasOurColours(final Enemy e) {
+		final RGB l = e.life;
+		return (withRed && l.R > 0) || (withGreen && l.G > 0)
+				|| (withBlue && l.B > 0);
+	}
 
 	@Override
 	public Enemy findSuitableEnemy(Tower t, List<Enemy> enemies) {
@@ -15,6 +33,9 @@ public class NearestEnemyPolicy implements EnemySelectionPolicy {
 		final float squaredRange = t.range * t.range;
 
 		for (Enemy e : enemies) {
+			// Skip enemies that don't contain the colours we can affect.
+			if (!enemyHasOurColours(e))
+				continue;
 			final float newEnemyLocationSquaredDist = t.location
 					.squaredDist(e.location);
 			// Within range?
--- src/com/avona/games/towerdefence/tower/RubyPrismaTower.java
+++ src/com/avona/games/towerdefence/tower/RubyPrismaTower.java
@@ -3,7 +3,9 @@ package com.avona.games.towerdefence.tower;
 import com.avona.games.towerdefence.RGB;
 import com.avona.games.towerdefence.TimedCodeManager;
 import com.avona.games.towerdefence.enemy.Enemy;
+import com.avona.games.towerdefence.enemySelection.EnemySelectionPolicy;
 import com.avona.games.towerdefence.enemySelection.NearestEnemyPolicy;
+import com.avona.games.towerdefence.enemySelection.NearestEnemyWithColourPolicy;
 import com.avona.games.towerdefence.particle.Particle;
 import com.avona.games.towerdefence.particleCollidors.NearestEnemyCollidorPolicy;
 
@@ -13,13 +15,20 @@ public class RubyPrismaTower extends Tower {
 
 	public RubyPrismaTower(final TimedCodeManager timedCodeManager,
 			final int level) {
-		super(timedCodeManager, new NearestEnemyPolicy(),
+		super(timedCodeManager, getPolicyForLevel(level),
 				new NearestEnemyCollidorPolicy(), level);
 		strength = new RGB(level * 10 + 10, 0, 0);
 	}
 
 	public RubyPrismaTower(final RubyPrismaTower other) {
 		super(other);
+	}
+
+	private static EnemySelectionPolicy getPolicyForLevel(int level) {
+		if (level > 1) {
+			return new NearestEnemyWithColourPolicy(true, false, false);
+		}
+		return new NearestEnemyPolicy();
 	}
 
 	@Override
