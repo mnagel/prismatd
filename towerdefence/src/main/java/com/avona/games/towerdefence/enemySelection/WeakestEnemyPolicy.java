a1cdd34b42d754be2e68fd51e533b1cefcba4cc6
Michael Nagel <michael.nagel@devzero.de>
2017-04-14 23:43:35 +0200

    Add a weakest enemy selection policy.

--- towerdefence/src/main/java/com/avona/games/towerdefence/enemySelection/WeakestEnemyPolicy.java
+++ towerdefence/src/main/java/com/avona/games/towerdefence/enemySelection/WeakestEnemyPolicy.java
@@ -1,8 +1,29 @@
 package com.avona.games.towerdefence.enemySelection;
 
-/**
- * Created by mna on 14.04.17.
- */
+import com.avona.games.towerdefence.enemy.Enemy;
+import com.avona.games.towerdefence.tower.Tower;
+
+import java.util.List;
 
 public class WeakestEnemyPolicy implements EnemySelectionPolicy {
+    @Override
+    public Enemy findSuitableEnemy(Tower tower, List<Enemy> enemies) {
+        Enemy bestEnemy = null;
+        float bestEnemyHealth = Float.MAX_VALUE;
+        final float squaredRange = tower.getRange() * tower.getRange();
+
+        for (Enemy e : enemies) {
+            final float newEnemyLocationSquaredDist = tower.location
+                    .squaredDist(e.location);
+            // Within range?
+            if (newEnemyLocationSquaredDist < squaredRange) {
+                // Shoot to nearest enemy.
+                if (bestEnemyHealth > e.life.length()) {
+                    bestEnemy = e;
+                    bestEnemyHealth = e.life.length();
+                }
+            }
+        }
+        return bestEnemy;
+    }
 }
